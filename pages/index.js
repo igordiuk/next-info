ea143cd8124a2d499ba70dd67b54c576c7192d2d
Igor Popadiuk <igordiuk@gmail.com>
2021-01-21 17:35:06 -0300

    Link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import { Link } from 'next/link'
+
 function Home() {
-  return <h1>Home</h1>
+  return (
+    <>
+      <h1>Home</h1>
+      
+      <Link href="/sobre">
+        <a>Acessar página Sobre</a>
+      </Link>
+    </>
+  )
 }
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,15 @@
+import { Link } from 'next/link'
+
+function Sobre() {
+  return (
+    <>
+      <h1>Sobre</h1>
+
+      <Link href="/">
+        <a>Acessar página Home</a>
+      </Link>
+    </>
+  )
+}
+
+export default Sobre
